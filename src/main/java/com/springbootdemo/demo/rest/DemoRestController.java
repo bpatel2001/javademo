e951433a114142f4a13eb4e1d0e6f141c93df5f7
bpatel2001 <bpatel2001h@gmail.com>
2025-06-11 15:00:04 -0500

    injected a custom application property in the controller

--- src/main/java/com/springbootdemo/demo/rest/DemoRestController.java
+++ src/main/java/com/springbootdemo/demo/rest/DemoRestController.java
@@ -1,12 +1,16 @@
 package com.springbootdemo.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
-    // exposing "/" that return "Hello World"
+    // Injecting a default company name from our application.properties
+    @Value("${company.name}")
+    private String companyName;
 
+    // Exposing "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
@@ -14,8 +18,6 @@ public class DemoRestController {
 
     @GetMapping("/carcompanies")
     public String returnCarCompanies() {
-        return "Honda, Toyota, BMW, Ford";
+        return companyName + ", Toyota, BMW, Ford";
     }
-
-
 }
