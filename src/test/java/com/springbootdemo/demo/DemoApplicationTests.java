2d53bc4b7f8fa2391569f8f789b5c9badcadcb65
bpatel2001 <bpatel2001h@gmail.com>
2025-06-11 14:47:16 -0500

    built a demo jar and was able to run it

--- src/test/java/com/springbootdemo/demo/DemoApplicationTests.java
+++ src/test/java/com/springbootdemo/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package demo.src.test.java.com.springbootdemo.demo;
+package com.springbootdemo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
